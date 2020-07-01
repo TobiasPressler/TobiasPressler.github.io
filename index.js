c6a3f9bc1a8e9b7ca14c1bb1bd70da6aba363829
TobiasPressler <pressler.tobias@gmail.com>
2020-07-01 11:28:22 +0200

    second try

--- index.js
+++ index.js
@@ -12,5 +12,17 @@ document.addEventListener("DOMContentLoaded", function () {
         scrollHorizontally: true
     });
 
+    VANTA.NET({
+        el: "#one",
+        mouseControls: true,
+        touchControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color: 0xf5f5f5,
+        backgroundColor: 0x13186b
+    })
+
 
 });
